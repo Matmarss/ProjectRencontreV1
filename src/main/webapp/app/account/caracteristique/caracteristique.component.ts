fe4eb91803dfac657efd7b73114d59a25219a1a5
Martin <martin.silvestre.fr@gmail.com>
2020-01-28 16:24:42 +0100

    premiere version de l'ajout des adjectifs

---
+++ src/main/webapp/app/account/caracteristique/caracteristique.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-caracteristique',
+  templateUrl: './caracteristique.component.html'
+})
+export class CaracteristiqueComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/main/webapp/app/account/caracteristique/caracteristique.route.ts
@@ -0,0 +1,14 @@
+import { Route } from '@angular/router';
+
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { CaracteristiqueComponent } from './caracteristique.component';
+
+export const caracteristiqueRoute: Route = {
+  path: 'caracteristique',
+  component: CaracteristiqueComponent,
+  data: {
+    authorities: ['ROLE_USER'],
+    pageTitle: 'Votre description'
+  },
+  canActivate: [UserRouteAccessService]
+};
